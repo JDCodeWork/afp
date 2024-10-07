501ff72d47b0b7447dc1142463e7eef9d72dd77b
Juan Orozco <jdorozco792006@gmail.com>
2024-10-06 22:38:12 -0500

    feat(Client and Api): :tada: Se agrego el codigo inicial para comenzar con el desarrollo del proyecto

---
+++ apps/api/README.md
@@ -0,0 +1,15 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+# Nest Postgres Template
+
+## Guía para desarrollo
+
+0. Clonar repositorio e instalar dependencias
+1. Renombrar el directorio principal según el proyecto
+2. Renombrar el archivo `.env.template` a `.env`
+3. Modificar las variables de entorno de acuerdo a las necesidades
+4. Modificar lo necesario en docker-compose, package.json
+5. Levantar la base de datos (`docker-compose up -d`)
+6. Iniciar la aplicación (`npm start:dev`)
---
+++ apps/api/src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AuthModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../', 'client/dist'),
+    }),
+  ],
+})
+export class AppModule {}
